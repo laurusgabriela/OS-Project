9b2d5ed5045f20e4be34f5f999b915829c689e9a
laurusgabriela <128591198+laurusgabriela@users.noreply.github.com>
2023-11-20 19:30:52 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -9,7 +9,7 @@ char="$1"
 count=0
 while IFS= read -r line || [ -n "$line" ];
 do
-    if [[ $line =~ '^[A-Z][a-zA-Z0-9 ,.!]*[A-Z]$' && ! $line =~ ',\ and\' ]];
+    if [[ $line =~ '^[A-Z][a-zA-Z0-9 ,.!]*["$char"][A-Z]$' && ! $line =~ ',\ and\' ]];
     then
 	((count++))
     fi
